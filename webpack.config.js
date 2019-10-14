88c2a487cea6379e7b9a82d230405dff935a3dbe
Wes <wesley.qb.wen@gmail.com>
2019-10-14 01:30:35 -0700

    Add initial webpack/babel setup files

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    entry: './client/index.jsx',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public')
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        }
+      ],
+      options: {
+        presets: ["@babel/preset-env", "@babel/preset-react"]
+      },
+    }
+  };
