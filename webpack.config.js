742c9f3b1e4ebcc10d5974134e11c327d973c363
Wes <wesley.qb.wen@gmail.com>
2019-11-30 17:22:34 -0800

    Minor updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,24 @@
 const path = require('path');
 module.exports = {
-    entry: './client/index.jsx',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'public')
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
-            options: {
-                presets: ["@babel/preset-env", "@babel/preset-react"]
-            }
+  watch: true,
+  mode: "development",
+  entry: './client/index.jsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
           }
         }
-      ]
-    }
+      }
+    ]
+  }
   };
