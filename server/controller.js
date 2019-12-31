f52db6e90c61bf9a474006a7ed9711d5828c6b5c
Wes <wesley.qb.wen@gmail.com>
2019-12-30 17:17:19 -0800

    Updated courseCheck to include general and restricted seat options

--- server/controller.js
+++ server/controller.js
@@ -14,6 +14,7 @@ const checkIfCourseIsValid = (userFormData) => {
       .then((response) => {
         const html = response.data
         const $ = cheerio.load(html);
+        
         // Check if the course being requested is valid
         // Check if the course being requested is actually empty
         const list = [];
@@ -59,13 +60,13 @@ const checkIfCourseIsValid = (userFormData) => {
 const checkIfNumberExists = (userFormData) => {
   return new Promise((resolve, reject) => {
     let queryString = 
-    'SELECT * FROM course WHERE phone_number = $1 AND subject_code = $2 AND course_number = $3 AND section_number = $4';
+    'SELECT * FROM course WHERE phone_number = $1 AND subject_code = $2 AND course_number = $3 AND section_number = $4 AND general_seat = $5 AND restricted_seat = $6';
     let { subject, course, section, generalSeat, restrictedSeat, phoneNumber } = userFormData.data
     pool
     .connect()
     .then(client => {
       return client
-        .query(queryString, [phoneNumber, subject, course, section])
+        .query(queryString, [phoneNumber, subject, course, section, generalSeat, restrictedSeat])
         .then(res => {
           client.release()
           if (res.rows.length === 0) {
@@ -76,32 +77,34 @@ const checkIfNumberExists = (userFormData) => {
         })
         .catch(e => {
           client.release()
+          console.log(e);
           reject(e.stack);
         })
     })
   })
 }
 
-const registerNumber = (userFormData) => {
-  return new Promise((resolve, reject) => {
+const registerNumber = (userFormData) =>
+  new Promise((resolve, reject) => {
     let queryString = 
-    'INSERT INTO course(subject_code, course_number, section_number, phone_number) VALUES($1, $2, $3, $4)';
-    let { subject, course, section, generalSeat, restrictedSeat, phoneNumber } = userFormData.data
+    'INSERT INTO course(subject_code, course_number, section_number, phone_number, restricted_seat, general_seat) VALUES($1,$2,$3,$4,$5,$6)';
+    const { subject, course, section, generalSeat, restrictedSeat, phoneNumber } = userFormData.data;
     pool
     .connect()
     .then(client => {
       return client
-        .query(queryString, [subject, course, section, phoneNumber])
+        .query(queryString, [subject, course, section, phoneNumber, restrictedSeat, generalSeat])
         .then(res => {
+          client.release();
           resolve(res);
         })
         .catch(e => {
+          console.log(e);
           client.release()
           reject(e.stack);
         })
       })
-  })
-}
+    })
 
 const getAccounts = (subject, course, section) => {
   return new Promise((resolve, reject) => {
