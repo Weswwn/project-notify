9b0729ad178b7604fa1b2a335b0c63e2b3852885
Wes <wesley.qb.wen@gmail.com>
2020-01-05 23:04:12 -0800

    Implemented text message feature/function

--- server/smsController.js
+++ server/smsController.js
@@ -1,13 +1,16 @@
 const { client } = require('./twilioController.js');
 
-const sendSMSMessage = (phoneNumber, course) => {
+const sendSMSMessage = (phoneNumber, subject, courseNum, section, restricted_seat, general_seat) => {
+  console.log(Number(phoneNumber));
   client.messages
   .create({
-    body: `Hi! This is Project Notify. The course ${course}, you requested has a spot that just opened up. Hurry and get it!`,
+    body: `Hi! This is Project Notify. The course (${subject.toUpperCase(), section}) you requested has a spot that just opened up. Hurry and get it!`,
     from: '+12017293373',
-    to: `${phoneNumber}`
+    to: `${Number(phoneNumber)}`
   })
-  .then(message => console.log(message.sid));
+  .then(message => {
+    console.log(message.sid)
+  });
 }
 
 module.exports = {
