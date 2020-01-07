e286865d9c96e163bc3b8f737a811071cf9eacf6
Wes <wesley.qb.wen@gmail.com>
2020-01-07 11:03:29 -0800

    Fixed bug where SMS message included section number but not course number

--- server/smsController.js
+++ server/smsController.js
@@ -1,14 +1,17 @@
 const { client } = require('./twilioController.js');
+const { removeNotifiedCourses } = require('./removeNotifiedCourses.js');
 
 const sendSMSMessage = (phoneNumber, subject, courseNum, section, restricted_seat, general_seat) => {
-  console.log(Number(phoneNumber));
+  let capitalSubject = subject.toUpperCase();
+  console.log(capitalSubject);
   client.messages
   .create({
-    body: `Hi! This is Project Notify. The course (${subject.toUpperCase(), section}) you requested has a spot that just opened up. Hurry and get it!`,
+    body: `Hi! This is Project Notify. The course (${capitalSubject} ${courseNum} ${section}) you requested has a spot that just opened up. Hurry and get it!`,
     from: '+12017293373',
     to: `${Number(phoneNumber)}`
   })
   .then(message => {
+    console.log(message.sid);
     removeNotifiedCourses(subject, courseNum, section, restricted_seat, general_seat);
   });
 }
