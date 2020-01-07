d8f581bad108f274b8558f3877e9faff1b538029
Wes <wesley.qb.wen@gmail.com>
2020-01-07 11:02:16 -0800

    Implemention of function

--- server/removeNotifiedCourses.js
+++ server/removeNotifiedCourses.js
@@ -1,6 +1,24 @@
 const {pool} = require('../database/index.js');
 
 const removeNotifiedCourses = (subject, courseNum, section, restricted, general) => {
+  const queryString = 'delete FROM course WHERE subject_code = $1 AND course_number = $2 AND section_number = $3 AND restricted_seat = $4 AND general_seat = $5';
+  pool
+      .connect()
+      .then(client => {
+        return client
+          .query(queryString, [subject, courseNum, section, restricted, general])
+          .then(res => {
+            console.log('From removeNotifications:', res);
+            client.release();
+          })
+      })
+      .catch(e => {
+        client.release()
+        console.log(e);
+      })
+}
 
+module.exports =  {
+  removeNotifiedCourses: removeNotifiedCourses
 }
 
