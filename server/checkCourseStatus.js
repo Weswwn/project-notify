6f86be83b87eef9b5cf4158cc23d1197ece14088
Wes <wesley.qb.wen@gmail.com>
2020-01-10 15:27:06 -0800

    Adjusted interval time

--- server/checkCourseStatus.js
+++ server/checkCourseStatus.js
@@ -20,33 +20,32 @@ const checkCourseStatus = (courseArray) => {
         // Check if the course being requested is actually empty
         const list = [];
         $('td strong').each(function(i, e) {
-          list[i] = $(this).text();
+          list[i] = Number($(this).text());
         })
 
-        // if (list[2] == '0' || list[3] == '0') return console.log(i, subject, courseNum, section, restrictedSeats, generalSeats, 'still full'); 
         if (restrictedSeats && generalSeats) {
-          if (list[0] !== '0') {
+          if (list[0] > 0) {
             queryListOfNumbersForCourse(subject, courseNum, section, restrictedSeats, generalSeats);
-          } else {
-            console.log(i, subject, courseNum, section, restrictedSeats, generalSeats, 'still full');
+          // } else {
+          //   console.log(i, subject, courseNum, section, restrictedSeats, generalSeats, 'still full');
           }
         } else if (restrictedSeats) {
-          if (list[3] !== '0') {
+          if (list[3] > 0) {
             queryListOfNumbersForCourse(subject, courseNum, section, restrictedSeats, generalSeats);
-          } else {
-            console.log(i, subject, courseNum, section, restrictedSeats, generalSeats, 'still full');
+          // } else {
+          //   console.log(i, subject, courseNum, section, restrictedSeats, generalSeats, 'still full');
           }
         } else if (generalSeats) {
-          if (list[2] !== '0') {
+          if (list[2] > 0) {
             queryListOfNumbersForCourse(subject, courseNum, section, restrictedSeats, generalSeats);
-          } else {
-            console.log(i, subject, courseNum, section, restrictedSeats, generalSeats, 'still full');
+          // } else {
+          //   console.log(i, subject, courseNum, section, restrictedSeats, generalSeats, 'still full');
           }
         } else if (restrictedSeats === false && generalSeats === false) {
-          if (list[0] !== '0') {
+          if (list[0] > 0) {
             queryListOfNumbersForCourse(subject, courseNum, section, restrictedSeats, generalSeats);
-          } else {
-            console.log(i, subject, courseNum, section, restrictedSeats, generalSeats, 'still full');
+          // } else {
+          //   console.log(i, subject, courseNum, section, restrictedSeats, generalSeats, 'still full');
           }
         }
     })
